e136f475103a01af866e8dd9190293efb14e8b12
stevejwebb <stevejwebb@yahoo.com>
2019-08-21 09:50:33 +0100

    Second

--- main.cpp
+++ main.cpp
@@ -3,8 +3,21 @@ Open a file and use that
 */
 #include<iostream>
 
+class Utilities {
+  public:
+  void cls()
+  {
+      for (int i = 0; i < 120; i++)
+      {
+          std::cout << std::endl;
+      }
+      
+  }
+
+};
+
 int main(int argv, char *argv[]){
-    
+
 
     return 0;
 }
